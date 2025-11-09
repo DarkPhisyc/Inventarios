2953acc8660a905edc3e4b5faf540c49db1d3f50
DarkPhisyc <darkphisyc50@gmail.com>
2025-11-09 09:52:45 -0600

    Case 1 para resolver inventarios clasicos

--- inventarios.java
+++ inventarios.java
@@ -15,9 +15,32 @@ public class inventarios {
             opcion = sc.nextInt();
 
             switch(opcion) {
-                
+                case 1:
+                    System.out.println("Teclea el valor de la demanda diaria (D):");
+                    D = sc.nextInt();
+                    System.out.println("Teclea el valor del costo de pedido (K):");
+                    K = sc.nextInt();
+                    System.out.println("Teclea el valor del costo de mantenimiento (h):");
+                    h = sc.nextInt();
+                    System.out.println("Teclea los días que tarda el proveedor en entregar el pedido (L):");
+                    L = sc.nextInt();
+
+                    y = Math.sqrt((2 * K * D) / h);
+                    System.out.println("Valor de y*: " + y);
+                    t0 = y / D;
+                    System.out.println("Valor de t0: " + t0);
+                    n = (int) (L / t0);
+                    System.out.println("Valor de n: " + n);
+                    Le = L - (n * t0);
+                    System.out.println("Valor de Le: " + Le);
+                    Pr = Le * D;
+                    System.out.println("Valor de Pr: " + Pr);
+                    TCU = (K / y / D ) + (h * (y / 2));
+                    System.out.println("Valor de TCU: " + TCU);
+
+                    System.out.println("Debe comprar " + (int)y + " unidades cada " + n + " dias, o cuando el nivel de inventario descienda a " + (int)Pr + " unidades.");
+                    break;
             }
-            System.out.println("Debe comprar " + (int)y + " unidades cada " + n + " dias, o cuando el nivel de inventario descienda a " + (int)Pr + " unidades.");
-            } while (opcion != 3);
+        } while (opcion != 3);
     }
 }
