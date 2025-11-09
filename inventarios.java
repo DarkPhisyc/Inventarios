de21c4ebb3af3fa4b10603469ca04519793ac834
DarkPhisyc <darkphisyc50@gmail.com>
2025-11-09 10:27:01 -0600

    Solicitar datos al usuario para el inventario con descuento

--- inventarios.java
+++ inventarios.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class inventarios {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, opcion;
-        double K, D, L, y, h, t0, Le, Pr, TCU;
+        int nC, n, opcion;
+        double K, D, L, y, h, t0, Le, Pr, TCU, desc;
+        double[] C;
 
         do {
             System.out.println("\n \n    RESOLVER MODELOS DE INVENTARIOS");
@@ -17,6 +18,7 @@ public class inventarios {
 
             switch(opcion) {
                 case 1:
+                    System.out.println("\nMODELO DE INVENTARIO CLASICO");
                     System.out.println("Teclea el valor de la demanda diaria (D):");
                     D = sc.nextDouble();
                     System.out.println("Teclea el valor del costo de pedido (K):");
@@ -25,7 +27,6 @@ public class inventarios {
                     h = sc.nextDouble();
                     System.out.println("Teclea los días que tarda el proveedor en entregar el pedido (L):");
                     L = sc.nextDouble();
-
                     y = Math.sqrt((2 * K * D) / h);
                     System.out.println("Valor de y*: " + y);
                     t0 = y / D;
@@ -38,11 +39,23 @@ public class inventarios {
                     System.out.println("Valor de Pr: " + Pr);
                     TCU = (K / y / D ) + (h * (y / 2));
                     System.out.println("Valor de TCU: " + TCU);
-
-                    System.out.println("Debe comprar " + (int)y + " unidades cada " + n + " dias, o cuando el nivel de inventario descienda a " + (int)Pr + " unidades.");
+                    System.out.println("Debe comprar " + (int)y + " unidades cada " + (int)t0 + " dias, o cuando el nivel de inventario descienda a " + (int)Pr + " unidades.");
                     break;
+
                 case 2:
-                    System.out.println("Teclea el valor de la demanda diaria ");
+                    System.out.println("\nMODELO DE INVENTARIO CON DESCUENTO");
+                    System.out.println("Teclea el valor de la demanda diaria (D):");
+                    D = sc.nextDouble();
+                    System.out.println("Teclea el valor del costo de pedido (K):");
+                    K = sc.nextDouble();
+                    System.out.println("Teclea cuantos precios se manejan (C):");
+                    nC = sc.nextInt();
+                    for(int i = 0; i < nC; i++) {
+                        System.out.println("Teclea el valor de C" + (i + 1) + ":");
+                        C[i] = sc.nextDouble();
+                    }
+                    System.out.println("Teclea el valor del descuento (%) en decimales:");
+                    desc = sc.nextDouble();
             }
         } while (opcion != 3);
     }
