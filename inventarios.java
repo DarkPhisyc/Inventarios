1551c8cc7f620f5bf18d559eb34ad821f666d159
DarkPhisyc <darkphisyc50@gmail.com>
2025-11-13 19:20:23 -0600

    Correcciones y añadir la politica al modelo con descuento

--- inventarios.java
+++ inventarios.java
@@ -4,13 +4,13 @@ public class inventarios {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nC, n, m, opcion;
-        double K, D, L, y, h, t0, Le, Pr, TCU, i, mTCU;
-        double[] C, hD, yD, TCUd;
+        double K, D, L, y, h, t0, Le, Pr, TCU, i, mCTU;
+        double[] C, hD, yD, CTUd;
         do {
             System.out.println("\n \n    RESOLVER MODELOS DE INVENTARIOS");
             System.out.println("\t \tMenu:");
-            System.out.println("1. Resolver Modelo de Inventario Clasico:");
-            System.out.println("2. Resolver Modelo de Inventario con Descuento:");
+            System.out.println("1. Resolver Modelo de Inventario Clasico");
+            System.out.println("2. Resolver Modelo de Inventario con Descuento");
             System.out.println("3. Salir");
             System.out.println("\nTeclea la opcion que deseas realizar:");
             opcion = sc.nextInt();
@@ -23,7 +23,7 @@ public class inventarios {
                     K = sc.nextDouble();
                     System.out.println("Teclea el valor del costo de mantenimiento (h):");
                     h = sc.nextDouble();
-                    System.out.println("Teclea los días que tarda el proveedor en entregar el pedido (L):");
+                    System.out.println("Teclea los dias que tarda el proveedor en entregar el pedido (L):");
                     L = sc.nextDouble();
                     y = Math.sqrt((2 * K * D) / h);
                     System.out.println("\nValor de y*: " + y);
@@ -50,30 +50,42 @@ public class inventarios {
                     C = new double[nC];
                     hD = new double[nC];
                     yD = new double[nC];
-                    TCUd = new double[nC];
+                    CTUd = new double[nC];
                     for(int j = 0; j < nC; j++) {
                         System.out.println("Teclea el valor de C" + (j + 1) + ":");
                         C[j] = sc.nextDouble();
                     }
+                    System.out.println("Teclea cuantos dias tarda el proveedor en entregar el pedido (L):");
+                    L = sc.nextDouble();
                     System.out.println("Teclea el valor del descuento (%) en decimales:");
                     i = sc.nextDouble();
                     for(int j = 0; j < nC; j++) {
                         hD[j] = C[j] * i;
                         yD[j] = Math.sqrt((2 * K * D) / hD[j]);
-                        TCUd[j] = (D * C[j]) + ((D * K) / yD[j]) + (hD[j] * yD[j] / 2);
+                        CTUd[j] = (D * C[j]) + ((D * K) / yD[j]) + (hD[j] * yD[j] / 2);
                         System.out.println("\nh" + (j + 1) + ": " + hD[j]);
                         System.out.println("y" + (j + 1) + ": " + yD[j]);
-                        System.out.println("TCU" + (j + 1) + ": " + TCUd[j]);
+                        System.out.println("CTU" + (j + 1) + ": " + CTUd[j]);
                     }
-                    mTCU = TCUd[0];
+                    t0 = yD[0] / D;
+                    System.out.println("\nValor de t0: " + t0);
+                    n = (int) (L / t0);
+                    System.out.println("Valor de n: " + n);
+                    Le = L - (n * t0);
+                    System.out.println("Valor de Le: " + Le);
+                    Pr = Le * D;
+                    System.out.println("Valor de Pr: " + Pr);
+                    TCU = (K / yD[0] / D ) + (hD[0] * (yD[0] / 2));
+                    System.out.println("Valor de TCU: " + TCU);
+                    mCTU = CTUd[0];
                     m = 0;
                     for(int j = 1; j < nC; j++) {
-                        if(TCUd[j] < mTCU) {
-                            mTCU = TCUd[j];
+                        if(CTUd[j] < mCTU) {
+                            mCTU = CTUd[j];
                             m = j;
                         }
                     }
-                    System.out.println("\nDebe pedir C" + (m + 1) + " para un costo minimo total de " + mTCU);
+                    System.out.println("\nDebe pedir C" + (m + 1) + " para un costo minimo total de CTU: " + mCTU + "y un TCU de: " + TCU + ".");
                     break;
                 case 3:
                     System.out.println("\nSaliendo...");
@@ -86,3 +98,4 @@ public class inventarios {
         sc.close();
     }
 }
+
